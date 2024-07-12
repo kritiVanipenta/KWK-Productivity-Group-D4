fffcae93c2c37f8919b4bb58d93bd66ba817c641
rhapsody <kriti.vanipenta@gmail.com>
2024-07-12 13:25:58 -0700

    Initial Commit

---
+++ KWK Productivity Group D4/KWK_Productivity_Group_D4App.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_Productivity_Group_D4App.swift
+//  KWK Productivity Group D4
+//
+//  Created by Kriti Vanipenta on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_Productivity_Group_D4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
