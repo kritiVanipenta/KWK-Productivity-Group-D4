b624d5c9251524bed9b0e8c922afef9e31b25e66
Annie Han <annie.han222@gmail.com>
2024-07-17 13:49:58 -0700

    added menu page issues with text being inside pink boxes

--- KWK Productivity Group D4/Menu.swift
+++ KWK Productivity Group D4/Menu.swift
@@ -9,24 +9,57 @@ import SwiftUI
 
 struct Menu: View {
     var body: some View {
-        ZStack {
-            //add background image
-            NavigationStack {
+        NavigationStack {
+            
+            ZStack {
+                Image("Main_bg")
+                  .ignoresSafeArea()
+                
                 VStack {
+                    //the focus fruit title and the home button are centered.. how move to top?
                     Text("Focus Fruits")
                         //add modifiers
-                    NavigationLink(destination: timePicker()) {
-                        Text("Timer")
+                        .font(.custom("Times New Roman", size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color(red:0.97, green: 0.73, blue: 0.73))
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Image("home")
+                          .resizable()
+                          .cornerRadius(10)
+                          .frame(width: 90, height: 70)
                     }
-                    //add modifiers for time picker link
-                    NavigationLink(destination: summaryPage()) {
-                        Text("Summary")
-                    }
+                    .offset(x: -120, y: 0)
+                    
+                    Rectangle()
+                        .fill((Color(red:1, green: 0.43, blue: 0.53)))
+                        .frame(width: 120, height: 50)
+                        .cornerRadius(10)
+                    NavigationLink(destination: timePicker()){
+                            Text("Timer")
+                        }
+                    //modifiers for time picker link
+                    .foregroundColor(.white)
+                    .font(.custom("Times New Roman", size: 30))
+                    
+                    Rectangle()
+                        .fill((Color(red:1, green: 0.43, blue: 0.53)))
+                        .frame(width: 120, height: 50)
+                        .cornerRadius(10)
+                    NavigationLink(destination: summaryPage()){
+                            Text("Summary")
+                        }
                     //add modifiers for summary picker link
+                    .foregroundColor(.white)
+                    .font(.custom("Times New Roman", size: 30))
+                    
                 }
-                //add modifiers here for title
             }
-        }
+            
+    }
     }
 }
 
